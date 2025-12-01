33adb71fa8b0e6dfba3371339e1cccfeb012e738
Max <lamhayyu1234@gmail.com>
2025-12-01 17:58:36 +0800

    haha

---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    //Drop Notes
+    //Tool
+    //1. System.out.println("abc")
+
+    //Every line of Java lines must end with ;
+    System.out.println("Hello world!!!");
+    System.out.println("Hello world!!!");
+    System.out.println("Hello world!!!");
+
+  }
+}
