6b41593b96b5e01ac440e6f9cd293e1dc52827ed
Max <lamhayyu1234@gmail.com>
2025-12-02 15:57:24 +0800

    hi

--- demo-datatype/src/DemoMath.java
+++ demo-datatype/src/DemoMath.java
@@ -38,7 +38,84 @@ public class DemoMath {
     // short s20 = b10 + s10;
     int s20 = b10 + s10; // ok
 
-    //char -> int
+    int result = 10 / 3;
+    System.out.println(result); //3
+
+    double englishScore = 87;
+    int mathScore = 72;
+    int historyScore = 34;
+    
+    //Step 1: (int + int + int) / int -> int( answer:64)
+    //Step 2: assign int value 64 int double box ->
+    double averageScore = (double)(englishScore + mathScore + historyScore)/3.0;
+    System.out.println(averageScore); //64.3333333
+
+    //int count = 0;
+    //System.out.println(10 / count); //error
+
+    System.out.println(0.1 + 0.2);
+    double d1 = 0.1;
+    double d2 = 0.2;
+    double d3 = d1 + d2;
+    System.out.println(d3);
+    System.out.println(0.2+0.2);
+
+    // ! Never use double variable for math operations (+,-,*,/)
+    // ! Solution BigDecimal
+
+    // ! Overflow (No error)
+    int i10 = 2_100_000_000;
+    int i11 = i10 + 100_000_000;
+    System.out.println(i11);
+    // ! For primitive (int, long), it will not error after math operation even it exceed the upper/ lower limit
+
+    // ! Step 1 : int + long -> long (No overflow)
+    // ! Step 2 : assign long value into long variable
+    long l10 = i10 + 100_000_000L;
+    System.out.println(l10);
+
+    //char -> int (ASCII)
+    int v = 97;
+    char n = 'a';
+
+    int v2 = n;
+    System.out.println(v2); //97
+
+    // A
+    char n2 = 'A';
+    int v3 = n2;
+    System.out.println(v3);
+
+    long l20 = 3;
+    int v4 = '你';
+    System.out.println(v4);
+
+    int v5 = 20320;
+    //! Cannot assign higher level value into lower llevel type.
+    // char c20 = v5;
+
+    int value = 'A'; // char -> int
+
+    //65 -> 'A'
+
+    long l30 = 2_200_000_000L;
+    int v10 = (int)l30; //自負盈虧
+    System.out.println(v10); //-2094967296
+
+    //65 -> A
+    int v11 = 65;
+    char c80 = (char) v11;
+    System.out.println(c80); //A
+
+    int v12 = 65620;
+    char c81 = (char) v12;
+    System.out.println(c81); // T (65620 - 65535)
+
+
+
+
+
+
 
 
 
